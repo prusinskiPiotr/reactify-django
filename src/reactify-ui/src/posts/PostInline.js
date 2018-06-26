a953f42a62869588df11c144b7b1ff2762d6edc5
Piotr <prusinski.pio@gmail.com>
2018-06-26 17:09:03 +0200

    Render post title and content on the page

--- src/reactify-ui/src/posts/PostInline.js
+++ src/reactify-ui/src/posts/PostInline.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 class PostInline extends Component {
   render() {
-
-    const {title} = this.props
+      const {post} = this.props
     return (
-        <div>
-            <h1>Post's {title}</h1>
-        </div>
+      <div>
+          {post !== undefined ? <div>
+              <h1>{post.title}</h1>
+              <p>{post.content}</p>
+              </div>
+              : ""}
+      </div>
     );
   }
 }
--- src/reactify-ui/src/posts/Posts.js
+++ src/reactify-ui/src/posts/Posts.js
@@ -5,70 +5,86 @@ import cookie from 'react-cookies'
 import PostInline from './PostInline'
 
 class Posts extends Component {
-  loadPosts(){
-    const endpoint = '/api/posts/'
-    let lookupOptions = {
-      method: "GET",
-      headers: {
-        'Content-Type': 'application/json'
-      }
+    state = {
+        posts: []
     }
-
-    fetch(endpoint, lookupOptions)
-    .then(function(response){
-      return response.json()
-    }).then(function(responseData){
-      console.log(responseData)
-    }).catch(function(error){
-      console.log("error", error)
-    })
-  }
-
-  CreatePost(){
-    const endpoint = '/api/posts/'
-    const csrfToken = cookie.load('csrftoken')
-    let data = {
-      "slug": "",
-      "title": "",
-      "content": "",
-      "draft": false,
-      "publish": null
-    }
-    if (csrfToken !== undefined){
-      let lookupOptions = {
-        method: "POST",
+    loadPosts(){
+        const endpoint = '/api/posts/'
+        let thisComp = this
+        let lookupOptions = {
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json',
-          'X-CSRFToken': csrfToken
-        },
-        body: JSON.stringify(data),
-        credentials: 'include'
-      }
-      fetch(endpoint, lookupOptions)
-      .then(function(response){
-        return response.json()
-      }).then(function(responseData){
-        console.log(responseData)
-      }).catch(function(error){
-        console.log("error", error)
-      })
+            'Content-Type': 'application/json'
+        }
+        }
+
+        fetch(endpoint, lookupOptions)
+        .then(function(response){
+            return response.json()
+        }).then(function(responseData){
+            console.log(responseData)
+            thisComp.setState({
+                posts: responseData
+            })
+        }).catch(function(error){
+            console.log("error", error)
+        })
+    }
+
+    CreatePost(){
+        const endpoint = '/api/posts/'
+        const csrfToken = cookie.load('csrftoken')
+        let data = {
+            "slug": "",
+            "title": "",
+            "content": "",
+            "draft": false,
+            "publish": null
+        }
+        if (csrfToken !== undefined){
+            let lookupOptions = {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': csrfToken
+                },
+                body: JSON.stringify(data),
+                credentials: 'include'
+            }
+            fetch(endpoint, lookupOptions)
+            .then(function(response){
+                return response.json()
+            }).then(function(responseData){
+                console.log(responseData)
+            }).catch(function(error){
+                console.log("error", error)
+            })
     }
 
 
-  }
+    }
 
-  componentDidMount(){
-    this.loadPosts()
-  }
+    componentDidMount(){
+        this.setState({
+            posts: []
+        })
+        this.loadPosts()
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <PostInline title='Test Title'/>
-      </div>
-    );
-  }
+    render() {
+        const {posts} = this.state
+        return (
+        <div>
+            <h1>Hello World</h1>
+            {posts.length > 0 ? posts.map((postItem, index)=>{
+            return (
+                <PostInline post={postItem} />
+            )
+            }) : <p>No Posts Found</p>}
+        </div>
+        );
+    }
 }
 
 export default Posts;
+
