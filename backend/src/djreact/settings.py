8d4a611236aa79e6eb9d3d06f0293bf63fca6e6e
Piotr <prusinski.pio@gmail.com>
2018-08-30 13:51:26 +0200

    load posts to main page using axios

--- backend/src/djreact/settings.py
+++ backend/src/djreact/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'articles',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import Article from '../components/Article';
+import axios from 'axios';
+import Articles from '../components/Article';
 
 
 class ArticleList extends React.Component {
+
+    state = {
+        articles: []
+    }
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api')
+            .then(res => {
+                this.setState({
+                    articles: res.data
+                });
+                console.log(res.data);
+            })
+    }
+
     render() {
         return(
-            <Article/>
+            <Articles data={this.state.articles}/>
         )
     }
 }
