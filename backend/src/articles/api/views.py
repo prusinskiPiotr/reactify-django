24f764e56b2d51aede483dfccfb8da6559cdfabd
Piotr <prusinski.pio@gmail.com>
2018-08-31 14:40:31 +0200

    create api views for create, update, delete

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (ListAPIView, RetrieveAPIView, 
+    ListCreateAPIView, UpdateAPIView, DestroyAPIView)
 from articles.models import Article
 from .serializers import ArticleSerializer
 
@@ -9,5 +10,20 @@ class ArticleListView(ListAPIView):
 
 
 class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleCreateView(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleUpdateView(UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDeleteView(DestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
