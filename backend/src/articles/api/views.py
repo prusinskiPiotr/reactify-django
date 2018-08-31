3cb172bcf306a20c14e046b6be3b0cd44c743393
Piotr <prusinski.pio@gmail.com>
2018-08-31 14:56:55 +0200

    refactor api views to use drf viewset

--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,29 +1,9 @@
-from rest_framework.generics import (ListAPIView, RetrieveAPIView, 
-    ListCreateAPIView, UpdateAPIView, DestroyAPIView)
+from rest_framework import viewsets
 from articles.models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
-
-
-class ArticleDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
 
-
-class ArticleCreateView(ListCreateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleUpdateView(UpdateAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
-
-
-class ArticleDeleteView(DestroyAPIView):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
